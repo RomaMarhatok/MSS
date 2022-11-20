d1faf47ad4cf38806ddbde76c3826050d4f0520e
roma <roma.marhatok@yandex.by>
2022-11-20 15:18:18 +0300

    fix model property in UserDocumentFactory

--- MSSProject/userApp/tests/factories/user_document_factory.py
+++ MSSProject/userApp/tests/factories/user_document_factory.py
@@ -2,7 +2,7 @@ from factory import SubFactory
 from factory.django import DjangoModelFactory
 from faker import Faker
 
-from ...models import DocumentType
+from ...models import UserDocument
 from ..factories.document_type_factory import DocumentTypeFactory
 from ..factories.user_factory import UserFactory
 
@@ -11,7 +11,7 @@ fake = Faker()
 
 class UserDocumentFactory(DjangoModelFactory):
     class Meta:
-        model = DocumentType
+        model = UserDocument
 
     content = fake.pystr()
     doctype = SubFactory(DocumentTypeFactory)
