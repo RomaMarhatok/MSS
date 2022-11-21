3a825b8ff44c5280c90883e3e11f1ac27c0fc349
roma <roma.marhatok@yandex.by>
2022-11-21 11:42:45 +0300

    create base serializers

---
+++ MSSProject/userApp/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import DocumentType, Roles, User, UserDocument, UserPersonalInfo
+
+
+class RolesSerializer(ModelSerializer):
+    class Meta:
+        model = Roles
+        fields = ("name",)
+
+
+class DocumentTypeSerializer(ModelSerializer):
+    class Meta:
+        model = DocumentType
+        fields = ("document_type",)
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("username", "login", "password")
+
+
+class UserPersonalInfoSerializer(ModelSerializer):
+    user = UserSerializer(many=False, required=True)
+
+    class Meta:
+        model = UserPersonalInfo
+        fields = ("user", "image", "first_name", "second_name", "patronymic", "email")
+
+
+class UserDocumentSerializer(ModelSerializer):
+    user = UserSerializer(many=False, required=True)
+    doctype = DocumentTypeSerializer(many=False, required=True)
+
+    class Meta:
+        model = UserDocument
+        fields = ("user", "content", "doctype")
