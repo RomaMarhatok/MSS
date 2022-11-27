62a5c54ed4ac279799e92aacdb28ccdf85fdbc7d
roma <roma.marhatok@yandex.by>
2022-11-27 17:49:41 +0300

    add fixtures for new model

--- MSSProject/userApp/tests/fixtures/dict_fixtures.py
+++ MSSProject/userApp/tests/fixtures/dict_fixtures.py
@@ -20,20 +20,6 @@ def user_fixture(role_fixture) -> dict:
 
 
 @pytest.fixture
-def document_type_fixture() -> dict:
-    return {"document_type": fake.pystr()}
-
-
-@pytest.fixture
-def user_document_fixture(document_type_fixture, user_fixture) -> dict:
-    return {
-        "content": fake.pystr(),
-        "doctype": document_type_fixture,
-        "user": user_fixture,
-    }
-
-
-@pytest.fixture
 def user_personal_info_fixture(user_fixture) -> dict:
     return {
         "user": user_fixture,
@@ -42,4 +28,47 @@ def user_personal_info_fixture(user_fixture) -> dict:
         "second_name": fake.last_name(),
         "patronymic": fake.last_name(),
         "email": fake.email(),
+    }
+
+
+@pytest.fixture
+def user_document_fixture(user_fixture) -> dict:
+    return {
+        "content": fake.text(),
+        "user": user_fixture,
+    }
+
+
+@pytest.fixture
+def doctor_types_fixture():
+    return {"doctor_type": fake.pystr()}
+
+
+@pytest.fixture
+def doctor_fixture(user_fixture, doctor_types_fixture):
+    return {"user": user_fixture, "doctor_type": doctor_types_fixture}
+
+
+@pytest.fixture
+def patient_fixture(user_fixture):
+    return {"user": user_fixture}
+
+
+@pytest.fixture
+def image_for_analyzes_fixture():
+    return {
+        "image": fake.image_url(),
+        "description": fake.text(),
+    }
+
+
+@pytest.fixture
+def treatment_history_fixture(
+    doctor_fixture, patient_fixture, image_for_analyzes_fixture
+):
+    return {
+        "description": fake.text(),
+        "doctor": doctor_fixture,
+        "patient": patient_fixture,
+        "image": image_for_analyzes_fixture,
     }
