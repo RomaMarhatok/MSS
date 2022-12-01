91c6672df2f6784e702f465c2a6041d62372e8fc
roma <roma.marhatok@yandex.by>
2022-12-01 13:37:10 +0300

    add slug field and rename table name

--- MSSProject/userApp/models.py
+++ MSSProject/userApp/models.py
@@ -1,15 +1,33 @@
 from datetime import datetime
 from django.db import models
+from django.template.defaultfilters import slugify
+from .utils.slug_utils import generate_slug_from_str
 
 
 class Role(models.Model):
+    slug = models.SlugField(max_length=100)
     name = models.CharField("role name", max_length=100, unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        return super(Role, self).save(*args, **kwargs)
+
+    class Meta:
+        db_table = "role"
 
 
 class User(models.Model):
+    slug = models.SlugField(max_length=100)
     login = models.CharField("user login", max_length=100, unique=True)
     password = models.CharField("user password", max_length=100)
     role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = generate_slug_from_str(self.login)
+        return super(User, self).save(*args, **kwargs)
+
+    class Meta:
+        db_table = "user"
 
 
 def media_path_builder_for_user_info(instance, filename):
@@ -39,25 +57,45 @@ class UserPersonalInfo(models.Model):
     def __str__(self) -> str:
         return self.user.login
 
+    class Meta:
+        db_table = "user_personal_info"
+
 
 class UserDocument(models.Model):
     content = models.TextField("document content")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    class Meta:
+        db_table = "user_document"
+
 
 class DoctorType(models.Model):
+    slug = models.SlugField(max_length=100)
     doctor_type = models.CharField(
         "doctor profession name", max_length=100, unique=True
     )
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.doctor_type)
+        return super(DoctorType, self).save(*args, **kwargs)
+
+    class Meta:
+        db_table = "doctor_type"
 
 
 class Doctor(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    class Meta:
+        db_table = "doctor"
+
 
 class DoctorDoctorTypes(models.Model):
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     doctor_type = models.ForeignKey(DoctorType, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "doctor_doctor_type"
 
 
 class Patient(models.Model):
@@ -78,20 +116,36 @@ def media_path_builder_for_analyzes_images(instance, filename):
 
 class ImageForAnalyzes(models.Model):
     image = models.ImageField(
-        "image for anlayzes",
+        "image for analyzes",
         upload_to=media_path_builder_for_analyzes_images,
         null=True,
         blank=True,
     )
     description = models.TextField()
 
+    class Meta:
+        db_table = "image_for_analyzes"
+
 
 class TreatmentHistory(models.Model):
+    slug = models.SlugField()
     description = models.TextField()
     doctor = models.ForeignKey(Doctor, on_delete=models.SET_NULL, null=True)
     patient = models.ForeignKey(Patient, on_delete=models.SET_NULL, null=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = generate_slug_from_str(
+            self.patient.user.login
+        ) + generate_slug_from_str(self.doctor.user.login)
+        return super(TreatmentHistory, self).save(*args, **kwargs)
+
+    class Meta:
+        db_table = "treatment_history"
 
 
 class TreatmentHistoryImageForAnalyzes(models.Model):
     treatment_history = models.ForeignKey(TreatmentHistory, on_delete=models.CASCADE)
     image_for_analyzes = models.ForeignKey(ImageForAnalyzes, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "treatment_history_image_for_analyzes"
