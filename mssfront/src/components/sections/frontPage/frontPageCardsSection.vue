a420e839f079d33e4b7e540df5828ab2b79333fa
roma <roma.marhatok@yandex.by>
2022-12-04 16:11:38 +0300

    create home page

---
+++ mssfront/src/components/sections/frontPage/frontPageCardsSection.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from "vue";
+import frontPageCard from "../../cards/frontPageCard.vue"
+const title = ref("SOME TEXT")
+const text = ref("Lorem Ipsum is simply dummy text of the printing and typesetting industry.")
+</script>
+<template>
+    <div class="cards__container">
+        <front-page-card :text="text" :title="title" />
+        <front-page-card :text="text" :title="title" />
+        <front-page-card :text="text" :title="title" />
+    </div>
+</template>
+<style>
+.cards__container {
+    padding-left: 5%;
+    padding-right: 5%;
+    display: flex;
+    flex-direction: row;
+    padding-top: 2%;
+    justify-content: space-between;
+}
+</style>
