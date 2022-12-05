669760c8243c3b4959e3770324748e3d75d9ca5b
roma <roma.marhatok@yandex.by>
2022-12-05 12:46:42 +0300

    change base model for user model

--- MSSProject/userApp/models.py
+++ MSSProject/userApp/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from django.db import models
 from django.template.defaultfilters import slugify
 from .utils.slug_utils import generate_slug_from_str
+from django.contrib.auth.models import AbstractUser
 
 
 class Role(models.Model):
@@ -16,7 +17,7 @@ class Role(models.Model):
         db_table = "role"
 
 
-class User(models.Model):
+class User(AbstractUser):
     slug = models.SlugField(max_length=100)
     login = models.CharField("user login", max_length=100, unique=True)
     password = models.CharField("user password", max_length=100)
