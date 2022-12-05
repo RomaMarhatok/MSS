2b7cd258fd03bb3ee529bdc531824c6eb0d35993
roma <roma.marhatok@yandex.by>
2022-12-05 11:19:34 +0300

    setup AUTH_USER_MODEL in project settings for token authentication

--- MSSProject/MSSProject/settings.py
+++ MSSProject/MSSProject/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTH_USER_MODEL = "userApp.User"
 # Application definition
 
 INSTALLED_APPS = [
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework.authtoken",
     "corsheaders",
     "userApp",
 ]
