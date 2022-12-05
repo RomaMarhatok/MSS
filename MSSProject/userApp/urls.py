b8e098a817419651a6a5a0843f7926ada625bdbf
roma <roma.marhatok@yandex.by>
2022-12-05 12:12:12 +0300

    create registration view

--- MSSProject/userApp/urls.py
+++ MSSProject/userApp/urls.py
@@ -1,3 +1,9 @@
 from rest_framework.routers import DefaultRouter
+from django.urls import include, path
+from views import TokenRegistrationView
 
-urlpatterns = []
+urlpatterns = [
+    path(
+        "registration/", TokenRegistrationView.as_view(), name="token-user-registration"
+    )
+]
--- MSSProject/userApp/views.py
+++ MSSProject/userApp/views.py
@@ -1,20 +1,25 @@
 from rest_framework.viewsets import ViewSet
-from .models import Doctor, Patient
+from rest_framework.views import APIView
+from .models import Doctor, Patient, User
 from .serializers.doctor_serializer import DoctorSerializer
+from .serializers.user_serializer import UserSerializer
 from .serializers.patient_serializer import PatientSerializer
 from django.http import JsonResponse
-from rest_framework.request import Request
+from django.http import HttpRequest
+from rest_framework.authtoken.models import Token
+from django.db.models import QuerySet
+from rest_framework.permissions import AllowAny
 
 
 class DoctorViewSet(ViewSet):
     lookup_field = "slug"
     queryset = Doctor.objects.all()
 
-    def list(self, request: Request):
+    def list(self, request: HttpRequest):
         serializer = DoctorSerializer(instance=self.queryset, many=True)
         return JsonResponse(data=serializer.data)
 
-    def retrieve(self, request: Request, slug=None):
+    def retrieve(self, request: HttpRequest, slug=None):
         doctor = self.queryset.get(slug=slug)
         serializer = DoctorSerializer(instance=doctor)
         return JsonResponse(data=serializer.data)
@@ -24,11 +29,39 @@ class PatientViewSet(ViewSet):
     lookup_field = "slug"
     queryset = Patient.objects.all()
 
-    def list(self, request: Request):
+    def list(self, request: HttpRequest):
         serializer = PatientSerializer(instance=self.queryset, many=True)
         return JsonResponse(data=serializer.data)
 
-    def retrieve(self, request: Request, slug=None):
+    def retrieve(self, request: HttpRequest, slug=None):
         patient = self.queryset.get(slug=slug)
         serializer = PatientSerializer(instance=patient)
         return JsonResponse(data=serializer.data)
+
+
+class TokenRegistrationView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request: HttpRequest):
+        if request.method == "POST":
+            users: QuerySet = User.objects.filter(**request.POST)
+            if users.exists():
+                return JsonResponse(data={"errors": ["user already exist"]}, status=400)
+            if not users.exists():
+                serializer = UserSerializer(data=request.POST)
+                if serializer.is_valid():
+                    user = serializer.save()
+                    Token.objects.create(user=user)
+                    return JsonResponse(
+                        data={"message": "user was successful registrated"}, status=200
+                    )
+                return JsonResponse(
+                    data={"errors": list(serializer.errors)}, status=400
+                )
+
+
+class TokenAuthenticationView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request: HttpRequest):
+        pass
