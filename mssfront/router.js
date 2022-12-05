f119c68aba8d8419bd051b0706ba759fd7502417
roma <roma.marhatok@yandex.by>
2022-12-05 19:53:54 +0300

    add registration page to router

--- mssfront/router.js
+++ mssfront/router.js
@@ -1,9 +1,11 @@
 import {createRouter,createWebHashHistory} from "vue-router";
 import frontPage from "./src/pages/frontPage"
+import registrationPage from "./src/pages/registrationPage"
 const router = createRouter({
     history:createWebHashHistory(),
     routes:[
-        {path:"/",name:"Home",component:frontPage}
+        {path:"/",name:"Home",component:frontPage},
+        {path:"/registration/",name:"Registration",component:registrationPage}
     ]
 })
 export default router
