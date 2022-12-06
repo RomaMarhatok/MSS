09a88834127afee76240449725ef8c43960b81f9
roma <roma.marhatok@yandex.by>
2022-12-06 20:20:38 +0300

    fix views request.POST on request.data

--- MSSProject/userApp/views.py
+++ MSSProject/userApp/views.py
@@ -4,7 +4,7 @@ from .models import Doctor, Patient, User
 from .serializers.doctor_serializer import DoctorSerializer
 from .serializers.user_serializer import UserSerializer
 from .serializers.patient_serializer import PatientSerializer
-from django.http import JsonResponse, HttpRequest, HttpResponse
+from django.http import JsonResponse, HttpRequest
 from rest_framework.authtoken.models import Token
 from django.db.models import QuerySet
 from rest_framework.permissions import AllowAny
@@ -42,39 +42,39 @@ class TokenRegistrationView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request: HttpRequest):
-        if request.method == "POST":
-            users: QuerySet = User.objects.filter(**request.POST)
-            if users.exists():
-                return JsonResponse(data={"errors": ["user already exist"]}, status=400)
-            else:
-                serializer = UserSerializer(data=request.POST)
-                if not serializer.is_valid():
-                    return JsonResponse(data={"errors": serializer.errors}, status=400)
-                user = serializer.save()
-                Token.objects.create(user=user)
-                Patient.objects.create(user=user)
-                return JsonResponse(
-                    data={"message": "user was successful registrated"}, status=200
-                )
+        users: QuerySet = User.objects.filter(
+            login=request.data["login"], password=request.data["password"]
+        )
+        if users.exists():
+            return JsonResponse(data={"errors": ["user already exist"]}, status=400)
+        else:
+            serializer = UserSerializer(data=request.data)
+            if not serializer.is_valid():
+                return JsonResponse(data={"errors": serializer.errors}, status=400)
+            user = serializer.save()
+            Token.objects.create(user=user)
+            Patient.objects.create(user=user)
+            return JsonResponse(
+                data={"message": "user was successful registrated"}, status=200
+            )
 
 
 class TokenAuthenticationView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request: HttpRequest):
-        if request.method == "POST":
-            users: QuerySet = User.objects.filter(
-                login=request.POST["login"], password=request.POST["password"]
+        users: QuerySet = User.objects.filter(
+            login=request.data["login"], password=request.data["password"]
+        )
+        if users.exists():
+            user = users.first()
+            user_auth_token_key = Token.objects.filter(user=user).first().key
+            return JsonResponse(
+                data={
+                    "message": "user successful authenticated",
+                    "token": user_auth_token_key,
+                    "user_role": user.role.name,
+                },
+                status=200,
             )
-            if users.exists():
-                user = users.first()
-                user_auth_token_key = Token.objects.filter(user=user).first().key
-                return JsonResponse(
-                    data={
-                        "message": "user successful authenticated",
-                        "token": user_auth_token_key,
-                        "user_role": user.role.name,
-                    },
-                    status=200,
-                )
-            return JsonResponse(data={"errors": "user don't exist"}, status=400)
+        return JsonResponse(data={"errors": "user don't exist"}, status=400)
