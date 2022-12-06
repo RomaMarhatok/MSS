b7a2c3205987014e6f935a3071396c6f6570c372
roma <roma.marhatok@yandex.by>
2022-12-06 13:09:18 +0300

    add forgot password link and fix style

--- mssfront/src/pages/registrationPage.vue
+++ mssfront/src/pages/registrationPage.vue
@@ -1,16 +1,21 @@
 <script setup>
 import registrationForm from '@/components/forms/registrationForm.vue';
+import registrationPageLink from '@/components/links/registrationPageLink.vue';
+import { ref } from 'vue';
+const changePasswordLink = ref("#/")
 </script>
 <template>
     <main class="main">
         <section class="section">
             <registrationForm />
         </section>
+        <registrationPageLink :link="changePasswordLink" />
     </main>
 </template>
 <style scoped>
 .main {
     display: flex;
+    flex-direction: column;
     font-family: 'Fira Sans';
     justify-content: center;
     align-items: center;
