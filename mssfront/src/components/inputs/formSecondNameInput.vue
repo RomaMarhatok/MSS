33ec30696411aca6b439d1309307324e288d9cb1
roma <roma.marhatok@yandex.by>
2022-12-07 23:54:26 +0300

    create inputs for text

--- mssfront/src/components/inputs/formSecondNameInput.vue
+++ mssfront/src/components/inputs/formSecondNameInput.vue
@@ -1,6 +1,6 @@
 <script setup>
 import formBaseInput from '@/components/inputs/base/formBaseInput.vue';
-import { reactive } from 'vue';
+import { reactive, defineProps } from 'vue';
 
 const passwordInput = reactive({
     labelText: "Second Name",
@@ -8,9 +8,12 @@ const passwordInput = reactive({
     inputType: "text",
     labelFor: "second_name_input",
 })
+const props = defineProps({
+    errors: Object,
+})
 </script>
 <template>
     <formBaseInput :labelText="passwordInput.labelText" :inputPlaceholder="passwordInput.inputPlaceholder"
         :inputType="passwordInput.inputType" :labelFor="passwordInput.labelFor"
-        @input="$emit('update:modelValue', $event.target.value)" />
+        @input="$emit('update:modelValue', $event.target.value)" :errors="props.errors" />
 </template>
