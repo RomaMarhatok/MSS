c1394866e0ee7519d8582baaa0fd9e1d71676213
roma <roma.marhatok@yandex.by>
2022-12-07 19:19:46 +0300

    create input for name

---
+++ mssfront/src/components/inputs/formSecondNameInput.vue
@@ -0,0 +1,16 @@
+<script setup>
+import formBaseInput from '@/components/inputs/base/formBaseInput.vue';
+import { reactive } from 'vue';
+
+const passwordInput = reactive({
+    labelText: "Second Name",
+    inputPlaceholder: "second name",
+    inputType: "text",
+    labelFor: "second_name_input",
+})
+</script>
+<template>
+    <formBaseInput :labelText="passwordInput.labelText" :inputPlaceholder="passwordInput.inputPlaceholder"
+        :inputType="passwordInput.inputType" :labelFor="passwordInput.labelFor"
+        @input="$emit('update:modelValue', $event.target.value)" />
+</template>
