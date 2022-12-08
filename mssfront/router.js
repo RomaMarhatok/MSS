46f5bd5a75c765a5439503f45d76b5d354cb23cd
roma <roma.marhatok@yandex.by>
2022-12-08 13:26:29 +0300

    create authentication page

--- mssfront/router.js
+++ mssfront/router.js
@@ -1,11 +1,13 @@
 import {createRouter,createWebHashHistory} from "vue-router";
 import homePage from "./src/pages/homePage"
 import registrationPage from "./src/pages/registrationPage"
+import authenticationPage from "./src/pages/authenticationPage"
 const router = createRouter({
     history:createWebHashHistory(),
     routes:[
         {path:"/",name:"Home",component:homePage},
-        {path:"/registration/",name:"Registration",component:registrationPage}
+        {path:"/registration/",name:"Registration",component:registrationPage},
+        {path:"/authentication/",name:"Authentication",component:authenticationPage}
     ]
 })
 export default router
---
+++ mssfront/src/pages/authenticationPage.vue
@@ -0,0 +1,34 @@
+<script setup>
+import registrationPageHeader from "@/components/headers/registrationPageHeader.vue"
+import authenticationForm from "@/components/forms/authenticationForm.vue";
+import registrationPageLink from '@/components/links/registrationPageLink.vue';
+import { ref } from 'vue';
+const changePasswordLink = ref("#/")
+</script>
+<template>
+    <main class="main">
+        <div class="wrapper">
+            <registrationPageHeader />
+            <authenticationForm />
+            <registrationPageLink :link="changePasswordLink" />
+        </div>
+
+    </main>
+</template>
+<style scoped>
+.main {
+    display: flex;
+    flex-direction: column;
+    font-family: 'Fira Sans';
+    justify-content: center;
+    align-items: center;
+    min-height: 85vh;
+    width: 100%;
+}
+
+.wrapper {
+    display: flex;
+    flex-direction: column;
+    width: 80%;
+}
+</style>
