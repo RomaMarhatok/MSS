3833d53ed574ba58c5c7af0ec4cbeacc389f8aba
roma <roma.marhatok@yandex.by>
2022-12-09 19:01:24 +0300

    remove style inport

--- mssfront/src/main.js
+++ mssfront/src/main.js
@@ -5,7 +5,6 @@ import PrimeVue from "primevue/config"
 import './index.css'
 import "primevue/resources/primevue.min.css"
 import "primeicons/primeicons.css"
-import "primevue/resources/themes/saga-blue/theme.css"
 
 const app = createApp(App)
 app.use(PrimeVue)
