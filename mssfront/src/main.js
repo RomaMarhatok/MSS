f69bce9868e45a00831cf50dfe2678fd44c3003f
roma <roma.marhatok@yandex.by>
2022-12-09 18:30:02 +0300

    setup tailwind and vueprime

--- mssfront/src/main.js
+++ mssfront/src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "../router.js"
+import PrimeVue from "primevue/config"
 import './index.css'
-createApp(App).use(router).mount('#app')
+import "primevue/resources/primevue.min.css"
+import "primeicons/primeicons.css"
+import "primevue/resources/themes/saga-blue/theme.css"
+
+const app = createApp(App)
+app.use(PrimeVue)
+app.use(router).mount('#app')
 
