a665ed6d00b527e0434498f2c9ca582418c0f94f
roma <roma.marhatok@yandex.by>
2022-12-10 15:53:58 +0300

    add route for documentPage

--- mssfront/router.js
+++ mssfront/router.js
@@ -2,12 +2,14 @@ import {createRouter,createWebHashHistory} from "vue-router";
 import homePage from "./src/pages/homePage"
 import registrationPage from "./src/pages/registrationPage"
 import authenticationPage from "./src/pages/authenticationPage"
+import documentsPage from './src/pages/documentsPage'
 const router = createRouter({
     history:createWebHashHistory(),
     routes:[
         {path:"/",name:"Home",component:homePage},
         {path:"/registration/",name:"Registration",component:registrationPage},
-        {path:"/authentication/",name:"Authentication",component:authenticationPage}
+        {path:"/authentication/",name:"Authentication",component:authenticationPage},
+        {path:"/user/documents/",name:"UserDocuments",component:documentsPage},
     ]
 })
 export default router
