720964e9959f40a97aa1d92e33b1034364e50019
roma <roma.marhatok@yandex.by>
2022-12-10 15:55:51 +0300

    create components for document page

---
+++ mssfront/src/components/headers/documentsPageHeader.vue
@@ -0,0 +1,11 @@
+<script setup>
+import documentPageNavBar from '../navbars/documentPageNavBar.vue';
+</script>
+<template>
+    <header class="flex flex-col">
+        <div class="flex bg-header-dark-blue w-full pl-3">
+            <div class="font-black text-3xl text-white underline decoration-3">MSS</div>
+        </div>
+        <documentPageNavBar />
+    </header>
+</template>
---
+++ mssfront/src/pages/documentsPage.vue
@@ -0,0 +1,11 @@
+<script setup>
+import documentsPageHeader from '@/components/headers/documentsPageHeader.vue';
+import documentsListSection from '@/components/sections/documentPage/documentsListSection.vue';
+</script>
+<template>
+    <documentsPageHeader />
+    <main class="flex justify-center flex-row gap-3 p-5">
+        <documentsListSection />
+        <section></section>
+    </main>
+</template>
