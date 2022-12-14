aa5fc543a1f0cd0e2d85b8d9b9dccaf9fe692fea
roma <roma.marhatok@yandex.by>
2022-12-14 17:06:07 +0300

    add styles to document display section component

--- mssfront/src/components/sections/documentPage/documentDisplaySection.vue
+++ mssfront/src/components/sections/documentPage/documentDisplaySection.vue
@@ -6,10 +6,19 @@ const props = defineProps({
 })
 </script>
 <template>
-    <div
-        class="grid-cols-5 grid-flow-row-dense gap-3 justify-center w-4/5 max-[600px]:grid-cols-1 max-[900px]:grid-cols-2 max-[1300px]:grid-cols-3">
-        <div v-if="props.documents">
-            <documentListPageCard v-for="(document, index) in props.documents" :key="index" :document="document" />
+    <main>
+        <div>
+            <div class="flex flex-row">
+                <font-awesome-icon icon="fa-solid fa-magnifying-glass" class="relative" />
+                <input class="m-0 p-3 pr-8 font-extrabold w-full rounded-2xl outline-none">
+            </div>
         </div>
-    </div>
+        <div
+            class="grid-cols-5 grid-flow-row-dense gap-3 justify-center w-4/5 max-[600px]:grid-cols-1 max-[900px]:grid-cols-2 max-[1300px]:grid-cols-3">
+            <div v-if="props.documents">
+                <documentListPageCard v-for="(document, index) in props.documents" :key="index" :document="document" />
+            </div>
+        </div>
+    </main>
+
 </template>
