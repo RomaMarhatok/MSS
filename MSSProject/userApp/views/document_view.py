a27d521cd23d393ffc6e84d289ae7ec623785e38
roma <roma.marhatok@yandex.by>
2022-12-15 18:45:56 +0300

    create retrieve and list method in document view

--- MSSProject/userApp/views/document_view.py
+++ MSSProject/userApp/views/document_view.py
@@ -4,46 +4,26 @@ from ..serializers.user_serializer import UserDocumentSerializer
 from ..models import UserDocument
 from django.http import JsonResponse, HttpRequest
 from ..permissions.is_user_authenticated import IsUserAuthenticated
-from rest_framework.authtoken.models import Token
-from ..services.serializer_service import SerializerService
+from ..models import User
 
 
 class DocumentView(GenericViewSet):
     permission_classes = [IsUserAuthenticated]
     lookup_field = "slug"
 
-    def list(self, request: HttpRequest, slug=None):
-        print(slug)
-        if "Authorization" in request.headers:
-            key = request.headers["Authorization"].split(" ")[1]
-            try:
-                user = Token.objects.get(key=key).user
-            except Token.DoesNotExist:
-                return JsonResponse(
-                    data={"errors": ["authentication token doesn't exist"]}
-                )
-            user_docuements = UserDocument.objects.filter(user=user)
-            serializer = UserDocumentSerializer(user_docuements, many=True)
-            if serializer.is_valid():
-                return JsonResponse(
-                    data={"user_documents": serializer.data}, status=status.HTTP_200_OK
-                )
+    def list(self, request: HttpRequest, user_slug=None):
+        user_docuements = UserDocument.objects.filter(user__slug=user_slug)
+        print(user_docuements.count())
+        serializer = UserDocumentSerializer(instance=user_docuements, many=True)
         return JsonResponse(
-            data={"errors": ["authentication token doesn't provided"]},
-            status=status.HTTP_400_BAD_REQUEST,
+            data={"user_documents": serializer.data},
+            status=status.HTTP_200_OK,
         )
 
-    # def create(self, request):
-    #     serializer_service = SerializerService(UserDocumentSerializer, request.data)
-    #     if serializer_service.errors is not None:
-    #         return JsonResponse(
-    #             data={"errors": serializer_service.errors},
-    #             status=status.HTTP_400_BAD_REQUEST,
-    #         )
-    #     return JsonResponse(data={}, status=status.HTTP_200_OK)
-
-    def retrieve(self, request, slug=None):
-        user_document = UserDocument.objects.filter(slug=slug).first()
+    def retrieve(self, request, user_slug=None, doc_slug=None):
+        user_document = UserDocument.objects.filter(
+            slug=doc_slug, user__slug=user_slug
+        ).first()
         serializer = UserDocumentSerializer(instance=user_document)
         return JsonResponse(
             data={"user_document": serializer.data}, status=status.HTTP_200_OK
