1b4d7e48ddecb33a15ee3acc8963ec20e735a212
roma <roma.marhatok@yandex.by>
2022-12-15 13:41:10 +0300

    change document display section

--- mssfront/src/components/sections/documentPage/documentDisplaySection.vue
+++ mssfront/src/components/sections/documentPage/documentDisplaySection.vue
@@ -6,11 +6,24 @@ const props = defineProps({
 })
 </script>
 <template>
-    <main>
-        <div>
-            <div class="flex flex-row">
-                <font-awesome-icon icon="fa-solid fa-magnifying-glass" class="relative" />
-                <input class="m-0 p-3 pr-8 font-extrabold w-full rounded-2xl outline-none">
+    <main class="mt-4 w-full">
+        <div class="flex flex-row justify-center">
+            <div class="search-box">
+                <button class="btn-search">
+                    <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
+                </button>
+                <input type="text" class="input-search" placeholder="Type to Search...">
+            </div>
+            <div class="flex flex-row justify-evenly gap-8">
+                <button class="display-button">
+                    <font-awesome-icon icon="fa-solid fa-table-cells" class="text-4xl" />
+                </button>
+                <button class="display-button">
+                    <font-awesome-icon icon="fa-solid fa-table-cells-large" class="text-4xl" />
+                </button>
+                <button class="display-button">
+                    <font-awesome-icon icon="fa-solid fa-list" class="text-4xl" />
+                </button>
             </div>
         </div>
         <div
@@ -20,5 +33,79 @@ const props = defineProps({
             </div>
         </div>
     </main>
-
 </template>
+<style scoped>
+.display-button {
+    display: flex;
+    padding: 5px;
+    font-size: 2em;
+    background: none;
+    border: none;
+    cursor: pointer;
+}
+
+.display-button:hover {
+    border-radius: 10px;
+    background-color: skyblue;
+}
+
+.search-box {
+    width: fit-content;
+    height: fit-content;
+    position: relative;
+}
+
+.input-search {
+    height: 50px;
+    width: 50px;
+    border-style: none;
+    padding: 10px;
+    font-size: 18px;
+    letter-spacing: 2px;
+    outline: none;
+    border-radius: 25px;
+    transition: all .5s ease-in-out;
+    background-color: white;
+    padding-right: 40px;
+    color: black;
+}
+
+.input-search::placeholder {
+    color: rgba(255, 255, 255, .5);
+    font-size: 18px;
+    letter-spacing: 2px;
+    font-weight: 100;
+}
+
+.btn-search {
+    width: 50px;
+    height: 50px;
+    border-style: none;
+    font-size: 20px;
+    font-weight: bold;
+    outline: none;
+    cursor: pointer;
+    border-radius: 50%;
+    position: absolute;
+    right: 0px;
+    color: black;
+    background-color: transparent;
+    pointer-events: painted;
+}
+
+.btn-search:focus~.input-search {
+    width: 300px;
+    border-radius: 0px;
+    background-color: transparent;
+    border-bottom: 1px solid rgba(11, 11, 11, 0.5);
+    transition: all 500ms cubic-bezier(0, 0.110, 0.35, 2);
+}
+
+.input-search:focus {
+    width: 300px;
+    border-radius: 0px;
+    background-color: transparent;
+    border-bottom: 1px solid rgba(11, 11, 11, 0.5);
+    transition: all 500ms cubic-bezier(0, 0.110, 0.35, 2);
+}
+</style>
