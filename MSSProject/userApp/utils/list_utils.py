00175d5fef692e53fa0d66de1592882de6f6d5ab
roma <roma.marhatok@yandex.by>
2022-12-17 15:22:05 +0300

    rewrite is_containe utils

--- MSSProject/userApp/utils/list_utils.py
+++ MSSProject/userApp/utils/list_utils.py
@@ -1,2 +1,2 @@
 def is_containe(validation_keys: list, checked_keys: list) -> bool:
-    return validation_keys == list(set(validation_keys) & set(checked_keys))
+    return set(validation_keys).issubset(set(checked_keys))
