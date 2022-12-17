0417f29215319477173c6e12fc3b365b2d3504c0
roma <roma.marhatok@yandex.by>
2022-12-17 15:32:00 +0300

    add middleware for authorization header

--- mssfront/src/baseApi.js
+++ mssfront/src/baseApi.js
@@ -4,4 +4,9 @@ const getBaseApi = axios.create({
     baseURL:"http://127.0.0.1:8000/mss",
     timeout:2000
 })
+getBaseApi.interceptors.request.use(function(config){
+    const token = localStorage.getItem("auth_token")
+    config.headers.Authorization = `Token ${token}`
+    return config
+})
 export default getBaseApi
