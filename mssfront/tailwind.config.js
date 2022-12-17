dbbcf23c24879c957318953427a25a26994a308b
roma <roma.marhatok@yandex.by>
2022-12-17 15:20:55 +0300

    add new tailwind styles

--- mssfront/tailwind.config.js
+++ mssfront/tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         "header-dark-blue":"#102039",
         "custom-menu-color":"#184ea5",
         "custom-deep-purple":"#4b32b0",
+        "error-message-bg":"#ffe7e6",
+        "success-message-bg":"#caf1d8",
+        "success-message-color":"#1ea97c",
       },
       minHeight: {
         '3/4': '80vh',
