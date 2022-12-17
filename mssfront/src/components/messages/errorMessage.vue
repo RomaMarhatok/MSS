4e71b500a4a8fe735d8844fa05ff7f987d40f88d
roma <roma.marhatok@yandex.by>
2022-12-17 15:24:42 +0300

    create custom messages

---
+++ mssfront/src/components/messages/errorMessage.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+    errorText: String
+})
+</script>
+<template>
+    <main class="bg-error-message-bg p-1 rounded-3xl">
+        <div class="pl-4">
+            <p class="text-red-600">{{ props.errorText }}</p>
+        </div>
+    </main>
+</template>
