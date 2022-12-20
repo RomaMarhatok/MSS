908a2a00a452a2fe2f976457e7196649fa5f23f1
roma <roma.marhatok@yandex.by>
2022-12-20 19:26:56 +0300

    add computed property for icon

--- mssfront/src/components/cards/documentListPageCard.vue
+++ mssfront/src/components/cards/documentListPageCard.vue
@@ -1,18 +1,31 @@
 <script setup>
-import { defineProps } from 'vue';
+import { defineProps, computed } from 'vue';
 const props = defineProps({
     document: Object
 })
+const getIcon = computed(() => {
+    let className = ""
+
+    if (props.document.document_type.name == "test") {
+        className = "fa-solid fa-flask"
+    }
+    if (props.document.document_type.name == "analyzes") {
+        className = "fa-solid fa-vials"
+    }
+    if (props.document.document_type.name == "conclusions") {
+        className = "fa-solid fa-file"
+    }
+    return className
+})
 </script>
 <template>
-    <button class="p-3 cursor-pointer bg-none border-none bg-white rounded-2xl hover:bg-sky-300">
+    <button class="p-3 cursor-pointer bg-none border-none bg-white rounded-2xl hover:bg-sky-300 hover:text-white">
         <div class="text-6xl">
-            <font-awesome-icon icon="fa-solid fa-flask" />
-            <font-awesome-icon icon="fa-solid fa-vials" />
+            <font-awesome-icon :icon="getIcon" />
         </div>
         <div>
             <p>
-                {{ props.document.content }}
+                {{ props.document.name }}
             </p>
         </div>
     </button>
