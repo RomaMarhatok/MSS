5ad43b67ea88876e9f6de08f7b01cde621ea1ba5
roma <roma.marhatok@yandex.by>
2022-12-20 15:29:24 +0300

    add slugify function in slug utils

--- MSSProject/userApp/utils/slug_utils.py
+++ MSSProject/userApp/utils/slug_utils.py
@@ -1,8 +1,9 @@
-from random import shuffle
 import re
+from django.template.defaultfilters import slugify
+from random import shuffle
 
 
 def generate_slug_from_str(string: str):
     string = [i for i in string if re.match("^[a-zA-Z0-9_]*$", i)]
     shuffle(string)
-    return "".join(string)
+    return slugify("".join(string))
