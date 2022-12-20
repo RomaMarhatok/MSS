3950735ca4d29345983f58a9cce8ce6d1849e408
roma <roma.marhatok@yandex.by>
2022-12-20 19:28:57 +0300

    create get user documents function

--- mssfront/services/UserService.js
+++ mssfront/services/UserService.js
@@ -3,5 +3,8 @@ class UserService extends RequestService {
     async getUserPersonalInfo(slug){
         return await this.post(`/mss/user/${slug}/profile/`)
     }
+    async getUserDocuments(slug){
+        return await this.get(`/mss/user/${slug}/documents/`)
+    }
 }
 export default UserService
