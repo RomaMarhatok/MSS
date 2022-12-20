45dcaa0852f53268a207a96b41495142d3c7523d
roma <roma.marhatok@yandex.by>
2022-12-20 19:28:36 +0300

    add request processing

--- mssfront/src/pages/userPages/documentsPage.vue
+++ mssfront/src/pages/userPages/documentsPage.vue
@@ -2,11 +2,24 @@
 import documentsPageHeader from '@/components/headers/documentsPageHeader.vue';
 import documentPageNavBar from '@/components/navbars/documentPageNavBar.vue';
 import documentDisplaySection from '@/components/sections/userPages/documentPage/documentDisplaySection.vue';
+import UserService from '@/../services/UserService';
+import { useRoute } from 'vue-router';
+import { ref, onMounted } from 'vue';
+const route = useRoute()
+const documents = ref([])
+onMounted(() => {
+    const userService = ref(new UserService())
+    userService.value.getUserDocuments(route.params.slug).then(response => {
+        documents.value = response.data.user_documents
+    }).catch(error => {
+        console.log(error)
+    })
+})
 </script>
 <template>
     <div>
         <documentsPageHeader />
         <documentPageNavBar />
-        <documentDisplaySection />
+        <documentDisplaySection :documents="documents" />
     </div>
 </template>
