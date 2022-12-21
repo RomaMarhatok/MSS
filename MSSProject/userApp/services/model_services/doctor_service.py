18bf2aa204e4e9a3c090415eb1718504c7aff7f8
roma <roma.marhatok@yandex.by>
2022-12-21 18:38:00 +0300

    added additional information which send document service

---
+++ MSSProject/userApp/services/model_services/doctor_service.py
@@ -0,0 +1,18 @@
+from .user_service import UserService
+from userApp.models import Doctor
+
+
+class DoctorService(UserService):
+    def get_doctor_info(self, slug):
+        user_personal_info = self.get_user_personal_info(slug)
+        doctor = Doctor.objects.filter(user__slug=slug).first()
+        doctor_types = self.get_doctor_types(doctor)
+        return {
+            "first_name": user_personal_info["first_name"],
+            "second_name": user_personal_info["second_name"],
+            "patronymic": user_personal_info["patronymic"],
+            "doctor_types": doctor_types,
+        }
+
+    def get_doctor_types(self, doctor: Doctor):
+        return [i.doctor_type.doctor_type for i in doctor.doctor_doctor_types.all()]
