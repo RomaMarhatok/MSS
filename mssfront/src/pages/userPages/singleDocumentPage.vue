e12dd0656175c18af074cd920e43806420e41118
roma <roma.marhatok@yandex.by>
2022-12-22 13:21:34 +0300

    clean up responce processing

--- mssfront/src/pages/userPages/singleDocumentPage.vue
+++ mssfront/src/pages/userPages/singleDocumentPage.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { reactive, onMounted, ref } from 'vue';
 import UserService from '@/../services/UserService';
+import baseLink from '@/components/links/Base/baseLink.vue';
 import { useRoute } from 'vue-router';
 const document = reactive({
     document_name: "",
@@ -11,13 +12,11 @@ const document = reactive({
     updated_at: "",
 })
 const route = useRoute()
+const redirectHref = ref(`#/user/${route.params.userSlug}/documents/`)
 onMounted(() => {
     const userService = ref(new UserService())
     const userSlug = route.params.userSlug
     const documentSlug = route.params.documentSlug
-    console.log(route.params.user_slug)
-    console.log(route.params.doc_slug)
-    console.log(userService)
     userService.value.getUserDocument(userSlug, documentSlug).then(response => {
         console.log(response)
 
@@ -25,23 +24,35 @@ onMounted(() => {
         const doctorCreator = response.data.doctor_creator
         document.document_name = userDocument.name
         document.document_type = userDocument.document_type.name
-        document.doctor_name = doctorCreator.first_name + " " + doctorCreator.second_name + " " + (doctorCreator.patronymic ?? "")
+        document.doctor_name = getFullName(doctorCreator)
         document.content = userDocument.content
-        document.created_at = userDocument.created_at
-        document.updated_at = userDocument.updated_at
+        document.created_at = "created at " + getFullDate(userDocument.created_at)
+        document.updated_at = "updated at " + getFullDate(userDocument.updated_at)
     }).then(error => {
         console.log(error)
     })
 })
+function getFullDate(dateString) {
+    const date = new Date(dateString)
+    const dd = date.getDate()
+    const mm = date.getMonth()
+    const yyyy = date.getFullYear()
+    return dd + "/" + mm + "/" + yyyy
+}
+function getFullName(doctorData) {
+    return "creator " + doctorData.first_name + " " + doctorData.second_name + " " + (doctorData.patronymic ?? "")
+}
 </script>
 <template>
     <div class="main">
         <div class="main-container">
             <div class="document-header">
                 <div class="content-container">
-                    <p>{{ document.document_name }}</p>
+                    <div class="header-container">
+                        <p>{{ document.document_type }}</p>
+                        <p>{{ document.document_name }}</p>
+                    </div>
                     <p>{{ document.doctor_name }}</p>
-                    <p>{{ document.document_type }}</p>
                     <p>{{ document.updated_at }}</p>
                     <p>{{ document.created_at }}</p>
                 </div>
@@ -49,9 +60,7 @@ onMounted(() => {
             <div class="main">
                 <p>{{ document.content }}</p>
             </div>
-            <div class="button-container">
-
-            </div>
+            <baseLink :text="'Back'" :href="redirectHref" />
         </div>
 
     </div>
@@ -60,14 +69,14 @@ onMounted(() => {
 .main {
     display: flex;
     justify-content: center;
+    padding: 2rem;
 }
 
 .main-container {
     display: flex;
     flex-direction: column;
-    box-shadow: 2px 4px 10px -4px rgba(34, 60, 80, 0.2);
+    box-shadow: 0px 0px 8px 0px rgba(34, 60, 80, 0.2);
     background-color: white;
-    font-size: 1.2em;
     margin-top: 10px;
     margin-bottom: 10px;
     width: 80%;
@@ -80,10 +89,22 @@ onMounted(() => {
     flex-direction: column;
     align-items: flex-start;
     word-break: break-all;
+    padding-top: 1rem;
 }
 
 .content-container {
+    display: flex;
+    flex-direction: column;
     padding-left: 20px;
+    width: 100%;
+
+}
+
+.header-container {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    gap: 1rem;
 }
 
 .content {
@@ -91,11 +112,5 @@ onMounted(() => {
     padding: 20px;
     margin-top: 10px;
     text-align: justify;
-}
-
-.button-container {
-    width: 100%;
-    display: flex;
-    justify-content: center;
 }
 </style>
