227518abff5976d0f87c9ac07cb3eed4d44b6bac
roma <roma.marhatok@yandex.by>
2022-12-23 17:38:39 +0300

    relocate user auth model

--- MSSProject/MSSProject/settings.py
+++ MSSProject/MSSProject/settings.py
@@ -31,7 +31,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-AUTH_USER_MODEL = "userApp.User"
 # Application definition
 
 INSTALLED_APPS = [
@@ -126,6 +125,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = "userApp.User"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
