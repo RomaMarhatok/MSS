cdd6b2aef49391096827d58daf461a5a285f1a26
roma <roma.marhatok@yandex.by>
2022-12-26 16:08:37 +0300

    add getDoctorBySlug method

--- mssfront/services/DoctorService.js
+++ mssfront/services/DoctorService.js
@@ -3,5 +3,9 @@ class DoctorService extends RequestService {
     async getAllDoctors(){
         return await this.get(`/mss/doctors/`)
     }
+
+    async getDoctorBySlug(slug){
+        return await this.get(`/mss/doctor/${slug}/`)
+    }
 }
 export default DoctorService
