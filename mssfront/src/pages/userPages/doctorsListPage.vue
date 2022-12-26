408738c5c07837f045c533cbf2f414f9467b5dac
roma <roma.marhatok@yandex.by>
2022-12-26 16:10:04 +0300

    up on one level above responce processing

--- mssfront/src/pages/userPages/doctorsListPage.vue
+++ mssfront/src/pages/userPages/doctorsListPage.vue
@@ -2,13 +2,21 @@
 import doctorsPageHeader from '@/components/headers/pageHeader.vue';
 import doctorsPageNavBar from '@/components/navbars/pageNavBar.vue';
 import doctorListSection from '@/components/sections/userPages/doctorsPage/doctorListSection.vue';
-import { ref } from 'vue';
+import DoctorService from '@/../services/DoctorService';
+import { ref, onMounted } from 'vue';
+const doctors = ref([])
 const activeLinks = ref([false, false, true])
+onMounted(() => {
+    const doctorService = ref(new DoctorService());
+    doctorService.value.getAllDoctors().then(response => {
+        doctors.value = response.data.doctors
+    }).catch(error => console.log(error))
+})
 </script>
 <template>
     <div class="flex flex-col gap-4">
         <doctorsPageHeader />
         <doctorsPageNavBar :activeLinks="activeLinks" />
-        <doctorListSection />
+        <doctorListSection :doctors="doctors" />
     </div>
 </template>
