85c6a11b7034370f58e77c734aaa7c0bde19c844
roma <roma.marhatok@yandex.by>
2022-12-27 12:58:20 +0300

    refactor get document method in document service

--- MSSProject/userApp/services/model_services/document_service.py
+++ MSSProject/userApp/services/model_services/document_service.py
@@ -1,6 +1,7 @@
 from ...models import Document, DocumentCreator
 from ...serializers.document_serializer import DocumentSerializer
 from ...services.model_services.doctor_service import DoctorService
+from ...serializers.document_creator_serializer import DocumentCreatorSerializer
 
 
 class DocumentService:
@@ -8,16 +9,10 @@ class DocumentService:
         user_document = Document.objects.filter(
             slug=document_slug, user__slug=user_slug
         ).first()
-        doctor = (
-            DocumentCreator.objects.filter(user_document=user_document).first().creator
-        )
-        doctor_service = DoctorService()
-        doctor_info = doctor_service.get_doctor_by_slug(doctor.user.slug)
-        serializer = DocumentSerializer(instance=user_document)
-        return {
-            "user_document": serializer.data,
-            "doctor_creator": doctor_info,
-        }
+        document_creator = DocumentCreator.objects.filter(
+            document=user_document
+        ).first()
+        return DocumentCreatorSerializer(instance=document_creator).data
 
     def get_all_documents(self, user_slug: str, include_context: bool = False):
         user_documents = Document.objects.filter(user__slug=user_slug)
