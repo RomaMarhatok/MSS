81780f3354597ac20f8ec55a78c2c8cc48c0db4d
roma <roma.marhatok@yandex.by>
2022-12-28 17:34:34 +0300

    update services under vuex

--- mssfront/services/RegistrationService.js
+++ mssfront/services/RegistrationService.js
@@ -1,7 +1,13 @@
 import RequestService from "./base/RequestService"
 class RegistrationService extends RequestService {
-    async registerUser(userData){
-        return await this.post("/mss/auth/registration/",userData)
+    async registerUser(userData,cb,errorCb){
+        return await this.post("/mss/auth/registration/",userData).then(response=>{
+            console.log("Registration response",response)
+            cb(response.data.message)
+        }).catch(error=>{
+            console.log("Registration error",error)
+            errorCb(error.response.data.errors)
+        })
     }
 }
 export default RegistrationService
