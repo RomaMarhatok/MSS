a1ee9945aefcaa96f1a5b941c3625343641944eb
roma <roma.marhatok@yandex.by>
2022-12-28 11:34:33 +0300

    add summary ref

--- mssfront/src/pages/userPages/singleDoctorPage.vue
+++ mssfront/src/pages/userPages/singleDoctorPage.vue
@@ -1,7 +1,7 @@
 <script setup>
 import doctorsPageHeader from '@/components/headers/pageHeader.vue';
 import doctorImageSection from '@/components/sections/userPages/doctorsPage/doctorImageSection.vue';
-// import doctorDataSection from '@/components/sections/userPages/doctorsPage/doctorDataSection.vue';
+import doctorDataSection from '@/components/sections/userPages/doctorsPage/doctorDataSection.vue';
 import DoctorService from '@/../services/DoctorService';
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
@@ -9,12 +9,14 @@ const route = useRoute()
 const doctorService = ref(new DoctorService())
 const fullName = ref("")
 const image = ref("")
+const summary = ref("")
 onMounted(() => {
     const doctorSlug = route.params.doctorSlug
     doctorService.value.getDoctorBySlug(doctorSlug).then((response) => {
         console.log(response)
         fullName.value = response.data.personal_info.full_name
         image.value = response.data.personal_info.image
+        summary.value = response.data.doctor_summary.summary
     }).catch((error) => console.log(error))
 
 })
@@ -22,7 +24,28 @@ onMounted(() => {
 <template>
     <div>
         <doctorsPageHeader />
-        <doctorImageSection :fullName="fullName" :image="image" />
-        <!-- <doctorDataSection /> -->
+        <main class="main-data-section">
+            <doctorImageSection :fullName="fullName" :image="image" />
+            <doctorDataSection :sectionTitle="'Resume'" :sectionText="summary" />
+        </main>
+
     </div>
 </template>
+<style scoped>
+.main-data-section {
+    display: flex;
+    flex-direction: row;
+    margin-top: 10px;
+    justify-content: space-evenly;
+}
+
+@media screen and (max-width: 1100px) {
+    .main-data-section {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 30px;
+        margin-top: 10px;
+    }
+}
+</style>
