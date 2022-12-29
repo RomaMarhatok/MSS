5f2efca1b7b451101507b7a6485f46da7881592c
roma <roma.marhatok@yandex.by>
2022-12-29 15:33:37 +0300

    add getters and actions for display data in vue components

--- mssfront/services/UserService.js
+++ mssfront/services/UserService.js
@@ -1,10 +1,18 @@
 import RequestService from "./base/RequestService"
 class UserService extends RequestService {
-    async getUserPersonalInfo(slug){
-        return await this.post(`/mss/user/${slug}/profile/`)
+    async getUserPersonalInfo(slug,cb,errorCb){
+        return await this.post(`/mss/user/${slug}/profile/`).then(response=>{
+            cb(response.data)
+        }).catch(error=>{
+            errorCb(error)
+        })
     }
-    async getUserDocuments(slug){
-        return await this.get(`/mss/user/${slug}/documents/`)
+    async getUserDocuments(slug,cb,errorCb){
+        return await this.get(`/mss/user/${slug}/documents/`).then(response=>{
+            cb(response.data.user_documents)
+        }).catch(error=>{
+            errorCb(error)
+        })
     }
     async getUserDocument(userSlug,documentSlug){
         return await this.get(`/mss/user/${userSlug}/document/${documentSlug}/`)
