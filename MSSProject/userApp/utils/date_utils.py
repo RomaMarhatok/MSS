8cf61d704aff43a3c053db546bdff21786e393ce
roma <roma.marhatok@yandex.by>
2023-01-08 15:06:20 +0300

    change return format of date in date utils

--- MSSProject/userApp/utils/date_utils.py
+++ MSSProject/userApp/utils/date_utils.py
@@ -2,12 +2,21 @@ import dateutil.parser
 from datetime import datetime
 
 
+def format_date(str_date):
+    return str_date if len(str_date) == 2 else "0" + str(str_date)
+
+
 def parse_date_iso_format(string) -> str:
     parsed_date: datetime = dateutil.parser.isoparse(string)
+
     return (
         str(parsed_date.day)
         + "/"
         + str(parsed_date.month)
         + "/"
         + str(parsed_date.year)
+        + " "
+        + format_date(str(parsed_date.hour))
+        + ":"
+        + format_date(str(parsed_date.minute))
     )
