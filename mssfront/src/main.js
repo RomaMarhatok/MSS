62ff70b9da5b2b1e79190880621aa21ae8f709de
roma <roma.marhatok@yandex.by>
2023-02-05 13:38:06 +0300

    add v-calendar to vue

--- mssfront/src/main.js
+++ mssfront/src/main.js
@@ -9,11 +9,15 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import store from "./store"
+import 'v-calendar/dist/style.css';
+import VCalendar from 'v-calendar';
+
 library.add(fas)
 const app = createApp(App)
 app.use(PrimeVue)
 app.use(router)
 app.use(store)
+app.use(VCalendar,{})
 app.component('font-awesome-icon', FontAwesomeIcon).mount('#app')
 
 
