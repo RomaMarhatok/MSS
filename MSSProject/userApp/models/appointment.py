4f7d3d5ab1be7091d460e6da6d18f9d0642a71bb
roma <roma.marhatok@yandex.by>
2023-02-09 15:19:51 +0300

    create new appoitments model

--- MSSProject/userApp/models/appointment.py
+++ MSSProject/userApp/models/appointment.py
@@ -1,10 +1,14 @@
 from django.db import models
 from .doctor import Doctor
 from .patient import Patient
+from .doctor_specialization import DoctorSpecialization
 
 
 class Appointments(models.Model):
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    doctor_specialization = models.ForeignKey(
+        DoctorSpecialization, on_delete=models.SET_NULL, null=True
+    )
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     date = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
