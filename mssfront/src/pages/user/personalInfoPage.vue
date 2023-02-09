3d276e15c0c64bf490a07b56d27cb2b7db512d39
roma <roma.marhatok@yandex.by>
2023-02-09 15:50:56 +0300

    add calendar section in profile page

--- mssfront/src/pages/user/personalInfoPage.vue
+++ mssfront/src/pages/user/personalInfoPage.vue
@@ -5,12 +5,14 @@ import { reactive, onBeforeMount, computed } from 'vue';
 import imageSection from '@/components/ui/Sections/UserPages/PersonalInfoPage/ImageSection.vue'
 import dataSection from '@/components/ui/Sections/UserPages/PersonalInfoPage/Base/DataSection.vue';
 import healthInfoSection from '@/components/ui/Sections/UserPages/PersonalInfoPage/HealthInfoSection.vue';
+import CalendarSection from '@/components/ui/Sections/UserPages/PersonalInfoPage/CalendarSection.vue';
 const store = useStore()
 const route = useRoute()
 const slug = computed(() => store.state.user.slug ? store.state.user.slug : route.params.userSlug)
 onBeforeMount(() => {
     console.log("on mounted")
     store.dispatch("user/fetchUserPersonalInfo", slug.value)
+    store.dispatch("user/fetchAppointments", slug.value)
 })
 
 const recentDocuments = reactive({
@@ -31,22 +33,34 @@ const imageSectionProps = reactive({
 </script>
 <template>
     <main>
-        <section class="flex__section">
+        <section class="flex__section flex_row">
             <healthInfoSection />
             <imageSection :links="imageSectionProps.links" />
         </section>
-        <section class="flex__section">
-            <dataSection v-if="recentDocuments.data.length" :headerText="recentDocuments.header"
-                :data="recentDocuments.data" />
-            <dataSection v-if="recentAppoitments.data.length" :headerText="recentAppoitments.header"
-                :data="recentAppoitments.data" />
+        <section class="flex__section flex_column">
+            <section class="flex__section flex_row">
+                <dataSection v-if="recentDocuments.data.length" :headerText="recentDocuments.header"
+                    :data="recentDocuments.data" />
+                <dataSection v-if="recentAppoitments.data.length" :headerText="recentAppoitments.header"
+                    :data="recentAppoitments.data" />
+            </section>
+            <CalendarSection></CalendarSection>
         </section>
+
     </main>
 </template>
 <style scoped>
+.flex_row {
+    flex-direction: row;
+
+}
+
+.flex_column {
+    flex-direction: column;
+}
+
 .flex__section {
     display: flex;
-    flex-direction: row;
     justify-content: center;
     gap: 30px;
     margin-top: 10px;
