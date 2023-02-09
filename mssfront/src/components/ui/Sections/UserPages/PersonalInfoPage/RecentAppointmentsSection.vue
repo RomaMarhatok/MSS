97dffd504f9ac1047d2c4ea36e6f4d162b37e411
roma <roma.marhatok@yandex.by>
2023-02-09 16:51:22 +0300

    add recent appointments section

---
+++ mssfront/src/components/ui/Sections/UserPages/PersonalInfoPage/RecentAppointmentsSection.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useStore } from 'vuex'
+import { reactive, computed } from 'vue'
+import DataSection from './Base/DataSection.vue';
+const store = useStore()
+const recentAppoitments = reactive({
+    header: "recent appoitments",
+    data: [],
+})
+recentAppoitments.data = computed(() => store.getters["user/getRecentAppointments"])
+</script>
+<template>
+    <DataSection v-if="recentAppoitments.data.length" :headerText="recentAppoitments.header"
+        :data="recentAppoitments.data" />
+</template>
