8a3c3ab2fe291d0b43ee94ceca6b0e4a67579fb1
roma <roma.marhatok@yandex.by>
2023-02-12 15:53:48 +0300

    add doctor and doctor type selector

---
+++ mssfront/src/components/common/Selectors/DoctorSelector.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { defineProps } from 'vue';
+import BaseSelector from './Base/BaseSelector.vue';
+const props = defineProps({
+    doctors: {
+        type: Array,
+        default: () => [],
+    },
+    modelValue: String
+})
+</script>
+<template>
+    <BaseSelector :value="props.modelVale" @change="$emit('update:modelValue', $event.target.value)"
+        :selectPlaceholder="'select doctor'">
+        <option v-for="doctor in props.doctors" :key="doctor.doctor_slug" :value="doctor.doctor_slug">{{
+            doctor.personal_info.full_name
+        }}
+        </option>
+    </BaseSelector>
+</template>
