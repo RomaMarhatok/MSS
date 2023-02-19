a79768c8b5a511085d04b7a5c8f4bd62423a20b6
roma <roma.marhatok@yandex.by>
2023-02-19 12:45:36 +0300

    rename section import

--- mssfront/src/pages/user/doctors/doctorsListPage.vue
+++ mssfront/src/pages/user/doctors/doctorsListPage.vue
@@ -1,7 +1,7 @@
 <script setup>
 import PageHeader from '@/components/layout/Headers/PageHeader.vue';
 import PageNavBar from '@/components/layout/Navbars/PageNavBar.vue';
-import DoctorListSection from '@/components/ui/Sections/UserPages/DoctorsPage/DoctorListSection.vue';
+import ListSection from '@/components/ui/Sections/UserPages/DoctorsPage/DoctorListSection.vue';
 import { ref, onBeforeMount } from 'vue';
 import { useStore } from 'vuex';
 const store = useStore()
@@ -15,6 +15,6 @@ onBeforeMount(() => {
     <div class="flex flex-col gap-4">
         <PageHeader />
         <PageNavBar :activeLinks="activeLinks" />
-        <DoctorListSection />
+        <ListSection />
     </div>
 </template>
