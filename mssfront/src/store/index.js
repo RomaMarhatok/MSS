c6003c97f8c405a294ebc6ecf5dc5954620fd07b
roma <roma.marhatok@yandex.by>
2023-02-20 15:22:59 +0300

    rewrite and devide stores

--- mssfront/src/store/index.js
+++ mssfront/src/store/index.js
@@ -2,16 +2,20 @@ import {createStore} from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
 import authentication from './modules/authentication'
 import registration from './modules/registration'
-import responseErrors from './modules/responseErrors'
 import user from './modules/user'
 import doctors from './modules/doctors'
+import response from './modules/response'
+import documents from './modules/documents'
+import appointments from './modules/appointments'
 const store = createStore({
     modules:{
         registration,
         authentication,
-        responseErrors,
         user,
         doctors,
+        response,
+        documents,
+        appointments,
     },
      plugins: [createPersistedState({
         storage: window.sessionStorage,
