881714f64c8f3f5ea8253ab954fbdbb3d70baded
roma <roma.marhatok@yandex.by>
2023-02-25 12:10:47 +0300

    add permission classes to view

--- MSSProject/userApp/views/doctor/patient_treatments_view.py
+++ MSSProject/userApp/views/doctor/patient_treatments_view.py
@@ -3,11 +3,14 @@ from django.http import HttpRequest, JsonResponse
 from userApp.services.model_services.treatment_history_service import (
     TreatmentHistoryService,
 )
+from userApp.permissions.is_doctor import IsDoctor
+from userApp.permissions.is_user_authenticated import IsUserAuthenticated
 
 
 class PatientTreatmentView(GenericViewSet):
+    permission_classes = [IsUserAuthenticated, IsDoctor]
+
     def list(self, request: HttpRequest, patient_slug: str):
-        print(1)
         service = TreatmentHistoryService()
         data = service.get_treatment_histories(patient_slug)
         return JsonResponse(
