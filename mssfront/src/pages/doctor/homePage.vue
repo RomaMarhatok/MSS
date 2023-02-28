28fee92772a458de4224df0cd57acc6d0af0173a
roma <roma.marhatok@yandex.by>
2023-02-28 10:18:22 +0300

    create doctor home page

---
+++ mssfront/src/pages/doctor/homePage.vue
@@ -0,0 +1,21 @@
+<script setup>
+import PageHeader from '@/components/layout/Headers/PageHeader.vue';
+import AppointmentsListSection from '@/components/ui/Sections/DoctorPage/AppointmentsListSection.vue';
+import TreatmentHistoryListSection from '@/components/ui/Sections/DoctorPage/TreatmentHistoryListSection.vue';
+import { onBeforeMount } from 'vue';
+import { useStore } from 'vuex';
+const store = useStore()
+onBeforeMount(() => {
+    store.dispatch("doctorAppointments/fetchAppointments", store.state.user.slug)
+})
+</script>
+<template>
+    <div class="flex flex-col">
+        <PageHeader></PageHeader>
+        <div class="flex flex-row m-2 gap-5">
+            <AppointmentsListSection></AppointmentsListSection>
+            <TreatmentHistoryListSection></TreatmentHistoryListSection>
+        </div>
+    </div>
+</template>
+<style lang="css"></style>
