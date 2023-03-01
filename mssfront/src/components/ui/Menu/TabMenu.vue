dc7c6b0d8d366d4c77eec6f3bc17e29cdd7ae416
roma <roma.marhatok@yandex.by>
2023-03-01 19:22:23 +0300

    create tab menu

---
+++ mssfront/src/components/ui/Menu/TabMenu.vue
@@ -0,0 +1,13 @@
+<script setup>
+import TabMenu from 'primevue/tabmenu';
+import { ref } from 'vue'
+const items = ref([
+    { label: 'Home', icon: 'pi pi-fw pi-home', to: "/home/" },
+    { label: 'Doctors', icon: 'pi pi-fw pi-calendar', to: "/doctors/" },
+    { label: 'Documents', icon: 'pi pi-fw pi-file', to: "/home/documents/" },
+    { label: 'Sign out', icon: 'pi pi-fw pi-sign-out', to: '/' }
+])
+</script>
+<template>
+    <TabMenu :model="items"></TabMenu>
+</template>
