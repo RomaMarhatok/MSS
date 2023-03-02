1ac6df75c35d2325db7b54fef8738b4201088298
roma <roma.marhatok@yandex.by>
2023-03-02 16:45:52 +0300

    change to representation image url to absolute image url

--- MSSProject/userApp/serializers/image_for_analyze_serializer.py
+++ MSSProject/userApp/serializers/image_for_analyze_serializer.py
@@ -1,5 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 from ..models import ImageForAnalyzes
+from django.core.handlers.wsgi import WSGIRequest
 
 
 class ImageForAnlyzeSerializer(ModelSerializer):
@@ -15,3 +16,13 @@ class ImageForAnlyzeSerializer(ModelSerializer):
             "created_at": {"required": False},
             "updated_at": {"required": False},
         }
+
+    def to_representation(self, instance: ImageForAnalyzes):
+        rep = super().to_representation(instance)
+        if (
+            instance.image.storage.exists(instance.image.name)
+            and "request" in self.context
+        ):
+            request: WSGIRequest = self.context["request"]
+            rep["image"] = request.build_absolute_uri(instance.image.url)
+        return rep
