dc97b51457b3e3ee3836c7affd7c6463113f179e
roma <roma.marhatok@yandex.by>
2023-03-04 10:42:59 +0300

    add status to cb in registration service

--- mssfront/services/RegistrationService.js
+++ mssfront/services/RegistrationService.js
@@ -2,7 +2,7 @@ import RequestService from "./base/RequestService"
 class RegistrationService extends RequestService {
     async registerUser(userData,cb,errorCb){
         return await this.post("/mss/auth/registration/",userData).then(response=>{
-            cb(response.data.message)
+            cb(response.status,response.data.message)
         }).catch(error=>{
             console.log("Registration error",error)
             errorCb(error.response.data.errors)
