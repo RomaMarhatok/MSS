72bfcb41dd9285c3b1964fcd34c01b9630fa6382
roma <roma.marhatok@yandex.by>
2023-03-05 13:01:12 +0300

    rename method and add retrieve method for goctor specialization

--- MSSProject/userApp/repositories/doctor_specialization_repository.py
+++ MSSProject/userApp/repositories/doctor_specialization_repository.py
@@ -3,5 +3,10 @@ from ..models import DoctorSpecialization
 
 
 class DoctorSpecializationRepository:
-    def get_all_doctor_types(self) -> QuerySet[DoctorSpecialization]:
+    def get_doctor_specializations(self) -> QuerySet[DoctorSpecialization]:
         return DoctorSpecialization.objects.all()
+
+    def get_doctor_specialization(
+        self, doctor_specialization_slug: str
+    ) -> QuerySet[DoctorSpecialization]:
+        return DoctorSpecialization.objects.filter(slug=doctor_specialization_slug)
