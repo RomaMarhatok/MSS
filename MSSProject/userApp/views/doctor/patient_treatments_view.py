599f33350a5839d70cc6b145157de7a907d9e2b4
roma <roma.marhatok@yandex.by>
2023-03-05 13:53:20 +0300

    change calls methods in views

--- MSSProject/userApp/views/doctor/patient_treatments_view.py
+++ MSSProject/userApp/views/doctor/patient_treatments_view.py
@@ -10,15 +10,24 @@ from userApp.permissions.is_user_authenticated import IsUserAuthenticated
 class PatientTreatmentView(GenericViewSet):
     permission_classes = [IsUserAuthenticated, IsDoctor]
 
-    def list(self, request: HttpRequest, patient_slug: str):
+    def list(
+        self, request: HttpRequest, patient_slug: str, doctor_specialization_slug: str
+    ):
         service = TreatmentHistoryService()
-        data = service.get_treatment_histories(patient_slug)
+        data = service.get_patient_treatment_histories(
+            patient_slug, doctor_specialization_slug
+        )
         return JsonResponse(
             data=data["data"],
             status=data["status"],
         )
 
-    def retrieve(self, request: HttpRequest, patient_slug: str, treatment_slug: str):
+    def retrieve(
+        self,
+        request: HttpRequest,
+        patient_slug: str,
+        treatment_slug: str,
+    ):
         service = TreatmentHistoryService()
         data = service.get_treatment_history(patient_slug, treatment_slug)
         return JsonResponse(
