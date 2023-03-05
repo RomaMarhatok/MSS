06fe7ef3eb0793effa4e62351eaf2cff982e3831
roma <roma.marhatok@yandex.by>
2023-03-05 13:50:58 +0300

    chnage treatment history repository get histories

--- MSSProject/userApp/repositories/treatment_history_repository.py
+++ MSSProject/userApp/repositories/treatment_history_repository.py
@@ -1,23 +1,36 @@
 from dataclasses import dataclass
 from ..models.treatment_history import TreatmentHistory
 from userApp.serializers.treatment_history_serializer import TreatmentHistorySerializer
+from userApp.repositories.doctor_repository import DoctorRepository
+from django.db.models import Q
 
 
 @dataclass
 class TreatmentHistoryRepository:
-    def get_treatments_histories_for_patient(self, patient_slug: str) -> list[dict]:
+    doctor_repository: DoctorRepository = DoctorRepository()
+
+    def get_treatments_histories(
+        self, patient_slug: str, doctor_specialization_slug: str
+    ):
+        doctors_slugs = self.doctor_repository.get_doctors_by_specialization(
+            doctor_specialization_slug
+        )
+
         treatment_histories = TreatmentHistory.objects.filter(
-            patient__user__slug=patient_slug
-        ).select_related("patient", "patient__user")
+            Q(patient__user__slug=patient_slug)
+            & Q(doctor__user__slug__in=doctors_slugs)
+        )
         serializer = TreatmentHistorySerializer(instance=treatment_histories, many=True)
         return serializer.data
 
-    def get_treatment_history_for_patient(
-        self, patient_slug: str, treatment_slug: str
-    ) -> dict:
-        treatment_history = TreatmentHistory.objects.filter(
-            patient__user__slug=patient_slug, slug=treatment_slug
-        ).first()
+    def get_treatment_history(self, patient_slug: str, treatment_slug: str) -> dict:
+        treatment_history = (
+            TreatmentHistory.objects.filter(
+                patient__user__slug=patient_slug, slug=treatment_slug
+            )
+            .select_related("patient", "patient__user", "patient__user__role")
+            .first()
+        )
         serializer = TreatmentHistorySerializer(instance=treatment_history)
         return serializer.data
 
