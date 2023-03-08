26f1e9080112a7c771b78fc9e2bf78e8ad216a45
roma <roma.marhatok@yandex.by>
2023-03-08 16:53:50 +0300

    change representation in appointment single section

--- mssfront/src/components/ui/Sections/DoctorPage/Appointments/AppointmentsSingleSection.vue
+++ mssfront/src/components/ui/Sections/DoctorPage/Appointments/AppointmentsSingleSection.vue
@@ -1,47 +1,95 @@
 <script setup>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useStore } from 'vuex';
 import AccordionTab from 'primevue/accordiontab';
 import Accordion from 'primevue/accordion';
 import Timeline from 'primevue/timeline'
 import Card from 'primevue/card';
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column';
+import SelectButton from 'primevue/selectbutton'
+import ScrollPanel from 'primevue/scrollpanel';
+import ScrollTop from 'primevue/scrolltop';
 import "primevue/resources/themes/saga-blue/theme.css"
 import "primevue/resources/primevue.min.css"
 const store = useStore()
 const getTreatmentsHistory = computed(() => {
     return store.getters["treatments/getTreatmentsHistories"]
 })
-console.log(getTreatmentsHistory.value)
-const currentAppointment = computed(() => store.state.treatments.selectedAppointment)
+const selectButtonOptions = ref(["list", "timeline"])
+const selectButtonValue = ref("list")
+const currentAppointment = computed(() => store.state.appointment.selectedAppointment)
 </script>
 <template>
     <main class="flex w-full p-6 flex-col">
-        <div>
-            Patient {{ currentAppointment.patient.user.full_name }}
-        </div>
-        <div v-for="(treatment, index) in getTreatmentsHistory" :key="index">
-            <Accordion>
-                <AccordionTab>
-                    <template #header>
-                        <div>
-                            {{ treatment.created_at }}
-                        </div>
-                    </template>
-                    <Timeline :value="getTreatmentsHistory" :align="'alternate'">
-                        <template #content="slotProps">
-                            <Card>
-                                {{ slotProps }}
-                            </Card>
-                        </template>
+        <div class="flex flex-row justify-between">
+            <p>
+                Patient {{ currentAppointment.patient.user.full_name }}
+            </p>
+            <SelectButton v-model="selectButtonValue" :options="selectButtonOptions" />
 
-                    </Timeline>
-                </AccordionTab>
-            </Accordion>
         </div>
+        <ScrollPanel v-if="selectButtonValue == 'timeline'" class="w-full m-3 border-y-3 pt-2 scroll-panel">
+            <Timeline v-if="selectButtonValue == 'timeline'" :value="getTreatmentsHistory" :align="'alternate'">
+                <template #opposite="slotProps">
+                    <small class="p-text-secondary">{{ slotProps.item.created_at }}</small>
+                </template>
+                <template #content="slotProps">
+                    <Card>
+                        <template #title>
+                            <p class="font-bold text-base text-left">
+                                {{ slotProps.item.title }}
+                            </p>
+                        </template>
+                        <template #content>
+                            <p class="text-left">
+                                {{ slotProps.item.short_description }}
+                            </p>
+                        </template>
+                    </Card>
+                </template>
+            </Timeline>
+            <ScrollTop target="parent" :threshold="100" class="custom-scroll-top" icon="pi pi-arrow-up" />
+        </ScrollPanel>
+
+        <DataTable v-if="selectButtonValue == 'list'" :value="getTreatmentsHistory" :rows="10" :paginator="true">
+            <template #empty>
+                No treatment histories found found.
+            </template>
+            <template #loading>
+                Loading patient data. Please wait.
+            </template>
+            <Column>
+                <template #header></template>
+                <template #body="{ data }">
+                    <Accordion>
+                        <AccordionTab>
+                            <template #header>
+                                <div>
+                                    {{ data.created_at }}
+                                </div>
+                            </template>
+                            <Card>
+                                <template #title>
+                                    <p class="font-bold text-base text-left">
+                                        {{ data.title }}
+                                    </p>
+                                </template>
+                                <template #content>
+                                    <p class="text-left">
+                                        {{ data.short_description }}
+                                    </p>
+                                </template>
+                            </Card>
+                        </AccordionTab>
+                    </Accordion>
+                </template>
+            </Column>
+        </DataTable>
     </main>
 </template>
 <style lang="css">
-.p-scrollpanel-bar {
+.p-scrollpanel .p-scrollpanel-bar {
     background-color: #3B82F6;
     opacity: 1;
     transition: background-color .2s;
@@ -54,6 +102,17 @@ const currentAppointment = computed(() => store.state.treatments.selectedAppoint
 }
 
 .scroll-panel {
-    height: 300px;
+    height: 550px;
+    max-height: 550px;
+}
+
+.p-scrolltop.p-link {
+    background-color: #3B82F6;
+
+}
+
+.p-scrolltop.p-link:hover {
+    background-color: #007ad9;
+
 }
 </style>
