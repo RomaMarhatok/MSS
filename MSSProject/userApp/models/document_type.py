2fe6520225550bd08cf2170952bad6e06f19cb04
roma <roma.marhatok@yandex.by>
2023-03-12 12:25:02 +0300

    add document type choice to document type model

--- MSSProject/userApp/models/document_type.py
+++ MSSProject/userApp/models/document_type.py
@@ -3,8 +3,18 @@ from ..utils.slug_utils import generate_slug_from_str
 
 
 class DocumentType(models.Model):
+    TEST = "TEST"
+    ANALYZES = "ANALYZES"
+    CONCLUSIONS = "CONCLUSIONS"
+
+    DOCUMENT_TYPE_CHOICES = (
+        (TEST, "Test"),
+        (ANALYZES, "Analyzes"),
+        (CONCLUSIONS, "Conclusions"),
+    )
+
     slug = models.SlugField(max_length=100, unique=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, choices=DOCUMENT_TYPE_CHOICES)
 
     def save(self, *args, **kwargs):
         self.slug = generate_slug_from_str(self.name)
