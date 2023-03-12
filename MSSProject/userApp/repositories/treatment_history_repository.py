fd583ad8a58b810057f35b15b5ceee5a17eec08c
roma <roma.marhatok@yandex.by>
2023-03-12 22:09:42 +0300

    remove comments

--- MSSProject/userApp/repositories/treatment_history_repository.py
+++ MSSProject/userApp/repositories/treatment_history_repository.py
@@ -57,28 +57,3 @@ class TreatmentHistoryRepository(AbstractRepository):
 
     def delete(self, **kwargs):
         return super().delete(**kwargs)
-
-    # def get_treatments_histories(
-    #     self, patient_slug: str, doctor_specialization_slug: str
-    # ):
-    #     doctors_slugs = self.doctor_repository.get_doctors_by_specialization(
-    #         doctor_specialization_slug
-    #     )
-
-    #     treatment_histories = TreatmentHistory.objects.filter(
-    #         Q(patient__user__slug=patient_slug)
-    #         & Q(doctor__user__slug__in=doctors_slugs)
-    #     ).order_by("-date")
-    #     serializer = TreatmentHistorySerializer(instance=treatment_histories, many=True)
-    #     return serializer.data
-
-    # def get_treatment_history(self, patient_slug: str, treatment_slug: str) -> dict:
-    #     treatment_history = (
-    #         TreatmentHistory.objects.filter(
-    #             patient__user__slug=patient_slug, slug=treatment_slug
-    #         )
-    #         .select_related("patient", "patient__user", "patient__user__role")
-    #         .first()
-    #     )
-    #     serializer = TreatmentHistorySerializer(instance=treatment_history)
-    #     return serializer.data
