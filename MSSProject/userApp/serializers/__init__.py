d68bc1dc039a5b51662a77d641029d401858b701
roma <roma.marhatok@yandex.by>
2023-03-12 19:36:56 +0300

    create init file for serializers

---
+++ MSSProject/userApp/serializers/__init__.py
@@ -0,0 +1,20 @@
+from .appointments_serializer import AppointmentsSerializer
+from .doctor_doctor_specialization_serializer import (
+    DoctorDoctorSpecializationSerializer,
+)
+from .doctor_serializer import DoctorSerializer
+from .doctor_specialization_serializer import DoctorSpecializationSerializer
+from .doctor_summary_serializer import DoctorSummarySerializer
+from .document_creator_serializer import DocumentCreatorSerializer
+from .document_serializer import DocumentSerializer
+from .document_type_serializer import DocumentTypeSerializer
+from .image_for_analyze_serializer import ImageForAnlyzeSerializer
+from .patient_serializer import PatientSerializer
+from .role_serializer import RoleSerializer
+from .treatment_history_image_for_analyze_serializer import (
+    TreatmentHistoryImageForAnalyzesSerializer,
+)
+from .treatment_history_serializer import TreatmentHistorySerializer
+from .user_location_serializer import UserLocationSerializer
+from .user_personal_info_serializer import UserPersonalInfoSerializer
+from .user_serializer import UserSerializer
