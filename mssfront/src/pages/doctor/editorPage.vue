c0bd565b43c321ce7602478473daf9ebd5047c9d
roma <roma.marhatok@yandex.by>
2023-03-13 16:49:17 +0300

    create editor page

---
+++ mssfront/src/pages/doctor/editorPage.vue
@@ -0,0 +1,8 @@
+<script setup>
+import EditorSection from '@/components/ui/Sections/DoctorPage/Editor/EditorSection.vue';
+</script>
+<template>
+    <main>
+        <EditorSection></EditorSection>
+    </main>
+</template>
