047d4ed20659b6fab5f0c5f08836b0019f16d789
roma <roma.marhatok@yandex.by>
2023-04-05 17:13:26 +0300

    fix role serializer

--- MSSProject/user/serializers/role_serializer.py
+++ MSSProject/user/serializers/role_serializer.py
@@ -1,18 +1,26 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import (
+    ChoiceField,
+    ModelSerializer,
+    SerializerMethodField,
+)
+
 from ..models import Role
 
 
 class RoleSerializer(ModelSerializer):
+    name = ChoiceField(
+        choices=(("DOCTOR", "Doctor"), ("PATIENT", "Patient")),
+        label="Role name",
+        validators=[],
+    )
+    slug = SerializerMethodField()
+
     class Meta:
         model = Role
         fields = (
             "name",
             "slug",
         )
-        extra_kwargs = {
-            "name": {
-                "validators": [],
-            },
-            "slug": {"required": False},
-            "url": {"lookup_field": "slug"},
-        }
+
+    def get_slug(self, instance: Role):
+        return instance.slug
