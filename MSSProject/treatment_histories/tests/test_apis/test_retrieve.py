fb999a38a7e2d205fb9888ef2d1abd321d385b0d
roma <roma.marhatok@yandex.by>
2023-04-07 15:55:30 +0300

    test treatment history

---
+++ MSSProject/treatment_histories/tests/test_apis/test_retrieve.py
@@ -0,0 +1,38 @@
+import pytest
+from django.urls import reverse
+from django.test.client import Client
+from rest_framework.authtoken.models import Token
+
+client = Client()
+
+
+@pytest.mark.django_db
+def test(factory_treatment_history_fixture):
+
+    url = reverse(
+        "treatment-history-retrieve",
+        args=[factory_treatment_history_fixture.slug],
+    )
+    token = Token.objects.create(user=factory_treatment_history_fixture.doctor.user).key
+    headers = {
+        "HTTP_AUTHORIZATION": "Bearer " + token,
+    }
+    response = client.get(url, **headers)
+    assert response.status_code == 200
+    assert bool(response.json())
+
+
+@pytest.mark.django_db
+def test_bad(factory_treatment_history_fixture):
+
+    url = reverse(
+        "treatment-history-retrieve",
+        args=["not-exist"],
+    )
+    token = Token.objects.create(user=factory_treatment_history_fixture.doctor.user).key
+    headers = {
+        "HTTP_AUTHORIZATION": "Bearer " + token,
+    }
+    response = client.get(url, **headers)
+    assert response.status_code == 400
+    assert bool(response.json())
