350cb0c09489f0df9b2b83fd6be0a2749ed14708
roma <roma.marhatok@yandex.by>
2023-04-07 15:56:10 +0300

    treatment history app verbose name

--- MSSProject/treatment_histories/apps.py
+++ MSSProject/treatment_histories/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class TreatmentHistoriesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "treatment_histories"
+    verbose_name = "Истории лечения"
