10f2fdfd90c9e2c568c06e5fa8a3a729f761e9d9
roma <roma.marhatok@yandex.by>
2023-04-07 17:11:31 +0300

    update document service

--- mssfront/services/DocumentService.js
+++ mssfront/services/DocumentService.js
@@ -4,10 +4,13 @@ class DocumentService extends RequestService{
     async getDocuments(slug,cb,errorCb){
         return await this.get(`/document/documents/${slug}/`).then(response=>{
             console.log(response.data.user_documents)
-            cb(response.status,response.data.user_documents)
+            cb(response.data.user_documents)
         }).catch(error=>{
             errorCb(error)
         })
+    }
+    async getDocument(slug,document_slug){
+        return await this.get(`/document/document/${slug}/${document_slug}/`)
     }
     async getDocumentTypes(cb,errorCb){
         return await this.get(`/document/documents/types/`).then(response=>{
