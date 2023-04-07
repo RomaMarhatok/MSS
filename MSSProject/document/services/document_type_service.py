78c2174dc31d7c47bd3a76bb26946b2052bd3e39
roma <roma.marhatok@yandex.by>
2023-04-07 12:31:06 +0300

    change return value

--- MSSProject/document/services/document_type_service.py
+++ MSSProject/document/services/document_type_service.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from django.http import JsonResponse
 from ..repositories import DocumentTypeRepository
 from ..serializers import DocumentTypeSerializer
 
@@ -8,8 +9,8 @@ class DocumentTypeService:
 
     document_type_repository: DocumentTypeRepository = DocumentTypeRepository()
 
-    def get_all_document_types(self) -> dict:
+    def get_all_document_types(self) -> JsonResponse:
         data = DocumentTypeSerializer(
             instance=self.document_type_repository.list(), many=True
         ).data
-        return {"document_types": data}
+        return JsonResponse(data={"document_types": data})
