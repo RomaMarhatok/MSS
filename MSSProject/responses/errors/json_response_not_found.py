a5f1ec67c8531e1d8ef83f08409e2026a28fd789
roma <roma.marhatok@yandex.by>
2023-04-09 19:21:13 +0300

    change not found response

--- MSSProject/responses/errors/json_response_not_found.py
+++ MSSProject/responses/errors/json_response_not_found.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
+from rest_framework import status
 
 
 class JsonResponseNotFound(JsonResponse):
-    status_code = 404
+    status_code = status.HTTP_404_NOT_FOUND
