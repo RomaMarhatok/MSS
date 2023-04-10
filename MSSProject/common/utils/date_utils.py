a5e533790fad0b71ec9466fb86b907aa98efd496
roma <roma.marhatok@yandex.by>
2023-04-11 01:20:20 +0300

    change date utils

--- MSSProject/common/utils/date_utils.py
+++ MSSProject/common/utils/date_utils.py
@@ -2,5 +2,26 @@ import dateutil.parser
 from datetime import datetime
 
 
-def parse_date_iso_format(string) -> datetime:
-    return dateutil.parser.isoparse(string)
+def parse_date_to_dict(string) -> dict:
+    dt = dateutil.parser.isoparse(string)
+    mounth_names = [
+        "Январь",
+        "Февраль",
+        "Март",
+        "Апрель",
+        "Май",
+        "Июнь",
+        "Июль",
+        "Август",
+        "Сентябрь",
+        "Октябрь",
+        "Ноябрь",
+        "Декабрь",
+    ]
+    return {
+        "mounth": mounth_names[dt.month - 1],
+        "day": dt.day if len(str(dt.day)) == 2 else "0" + str(dt.day),
+        "hours": dt.hour if len(str(dt.hour)) == 2 else "0" + str(dt.hour),
+        "minutes": dt.minute if len(str(dt.minute)) == 2 else "0" + str(dt.minute),
+        "year": dt.year,
+    }
