367efee0e9039559f5f836870f995030c21950e5
roma <roma.marhatok@yandex.by>
2023-04-13 14:09:38 +0300

    add short mounth names

--- MSSProject/common/utils/date_utils.py
+++ MSSProject/common/utils/date_utils.py
@@ -2,7 +2,7 @@ import dateutil.parser
 from datetime import datetime
 
 
-def parse_date_to_dict(string) -> dict:
+def parse_date_to_dict(string, short: bool = False) -> dict:
     dt = dateutil.parser.isoparse(string)
     mounth_names = [
         "Январь",
@@ -18,8 +18,24 @@ def parse_date_to_dict(string) -> dict:
         "Ноябрь",
         "Декабрь",
     ]
+    short_names = [
+        "Янв",
+        "Февр",
+        "Март",
+        "Апр",
+        "Мая",
+        "Июня",
+        "Июля",
+        "Авг",
+        "Сент",
+        "Окт",
+        "Нояб",
+        "Дек",
+    ]
     return {
-        "mounth": mounth_names[dt.month - 1],
+        "mounth": mounth_names[dt.month - 1]
+        if not short
+        else short_names[dt.month - 1],
         "day": dt.day if len(str(dt.day)) == 2 else "0" + str(dt.day),
         "hours": dt.hour if len(str(dt.hour)) == 2 else "0" + str(dt.hour),
         "minutes": dt.minute if len(str(dt.minute)) == 2 else "0" + str(dt.minute),
