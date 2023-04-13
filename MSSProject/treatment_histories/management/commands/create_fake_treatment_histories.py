d107aad81239bcc63debcabcc5813486d6bfa059
roma <roma.marhatok@yandex.by>
2023-04-13 15:59:12 +0300

    update tests

--- MSSProject/treatment_histories/management/commands/create_fake_treatment_histories.py
+++ MSSProject/treatment_histories/management/commands/create_fake_treatment_histories.py
@@ -1,4 +1,5 @@
 import random
+from datetime import datetime, timedelta
 from faker import Faker
 from typing import Any, Optional
 from django.core.management.base import BaseCommand
@@ -22,19 +23,22 @@ class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any) -> Optional[str]:
         users = User.objects.filter(role__name=Role.PATIENT)
         doctors = Doctor.objects.all()
-        for user, doctor in zip(users, doctors):
-            for _ in range(random.randint(1, 40)):
+        for user in users:
+            for _ in range(random.randint(1, 10)):
                 img_for_analyzes = ImageForAnalyzesFactory(
                     image=load_image_from_url(fake.image_url()),
-                    description=fake.text(max_nb_chars=10000),
+                    description=fake.text(max_nb_chars=100),
                 )
                 treatment = TreatmentHistoryFactory(
                     title=fake.text(max_nb_chars=100),
                     short_description=fake.text(max_nb_chars=100),
                     description=fake.text(max_nb_chars=1000),
                     conclusion=fake.text(max_nb_chars=100),
-                    date=fake.date_time(),
-                    doctor=doctor,
+                    date=fake.date_time_between(
+                        start_date=datetime.now(),
+                        end_date=datetime.now() + timedelta(days=300),
+                    ),
+                    doctor=random.choice(doctors),
                     patient=user,
                 )
                 TreatmentHistoryImageForAnalyzesFactory(
