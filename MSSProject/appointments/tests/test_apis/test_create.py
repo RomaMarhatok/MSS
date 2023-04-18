e16e24b8ecf5e723a60461d1535cf248ace5f9d1
roma <roma.marhatok@yandex.by>
2023-04-18 17:02:14 +0300

    add test for past and future time appointment app

--- MSSProject/appointments/tests/test_apis/test_create.py
+++ MSSProject/appointments/tests/test_apis/test_create.py
@@ -1,15 +1,11 @@
 import pytest
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.test.client import Client
 from django.urls import reverse
 from rest_framework.authtoken.models import Token
 from appointments.models import Appointments
 
 client = Client()
-# doctor_slug = serializers.SlugField()
-# patient_slug = serializers.SlugField()
-# doctor_specialization = serializers.SlugField()
-# date = serializers.DateTimeField()
 
 
 @pytest.mark.django_db
@@ -100,3 +96,47 @@ def test_patient_slug(
     assert response.status_code == 400
     assert bool(response.json())
     assert Appointments.objects.count() == 0
+
+
+@pytest.mark.django_db
+def test_past_time(
+    factory_user_with_role_patient_fixture,
+    factory_doctor_doctor_specialization_fixture,
+):
+    url = reverse("patient-appointments-create")
+    token = Token.objects.create(user=factory_user_with_role_patient_fixture).key
+    headers = {
+        "HTTP_AUTHORIZATION": "Bearer " + token,
+    }
+    data = {
+        "doctor_slug": factory_doctor_doctor_specialization_fixture.doctor.user.slug,
+        "patient_slug": factory_user_with_role_patient_fixture.slug,
+        "doctor_specialization_slug": factory_doctor_doctor_specialization_fixture.doctor_specialization.slug,
+        "date": datetime.now() - timedelta(days=10),
+    }
+    response = client.post(url, data, **headers)
+    assert response.status_code == 400
+    assert bool(response.json())
+    assert Appointments.objects.count() == 0
+
+
+@pytest.mark.django_db
+def test_future_time(
+    factory_user_with_role_patient_fixture,
+    factory_doctor_doctor_specialization_fixture,
+):
+    url = reverse("patient-appointments-create")
+    token = Token.objects.create(user=factory_user_with_role_patient_fixture).key
+    headers = {
+        "HTTP_AUTHORIZATION": "Bearer " + token,
+    }
+    data = {
+        "doctor_slug": factory_doctor_doctor_specialization_fixture.doctor.user.slug,
+        "patient_slug": factory_user_with_role_patient_fixture.slug,
+        "doctor_specialization_slug": factory_doctor_doctor_specialization_fixture.doctor_specialization.slug,
+        "date": datetime.now() + timedelta(days=10),
+    }
+    response = client.post(url, data, **headers)
+    assert response.status_code == 200
+    assert bool(response.json())
+    assert Appointments.objects.count() == 1
