a07fde69228140a8cc6a70a2a9a0d19511c35c96
roma <roma.marhatok@yandex.by>
2023-04-20 14:58:22 +0300

    update physical parameters

--- MSSProject/physical/models/physical_parameters.py
+++ MSSProject/physical/models/physical_parameters.py
@@ -1,12 +1,19 @@
 from django.db import models
 from user.models import User
+from common.utils.string_utils import generate_slug_from_str
 
 
 class PhysicalParameters(models.Model):
+    slug = models.SlugField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     weight = models.FloatField()
     height = models.FloatField()
     pressure = models.FloatField()
+    created_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = "physical_parameters"
+
+    def save(self, *args, **kwargs):
+        self.slug = generate_slug_from_str(self.user.slug)
+        return super().save(*args, **kwargs)
