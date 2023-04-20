caf8534eef459d5e3940ab57787b3c6711402b14
roma <roma.marhatok@yandex.by>
2023-04-20 13:08:39 +0300

    logout transition and profile tests

--- MSSProject/user/services/logout.py
+++ MSSProject/user/services/logout.py
@@ -1,7 +1,9 @@
+from django.db import transaction
 from rest_framework.authtoken.models import Token
 
 
 class LogOutService:
+    @transaction.atomic
     def logout(self, slug: str):
         if Token.objects.filter(user__slug=slug).exists():
             Token.objects.get(user__slug=slug).delete()
