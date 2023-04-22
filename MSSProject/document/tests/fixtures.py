39b0f4e6f1e3af4135e527e6264836f2cea42d3b
roma <roma.marhatok@yandex.by>
2023-04-22 16:41:50 +0300

    add tests for new model

--- MSSProject/document/tests/fixtures.py
+++ MSSProject/document/tests/fixtures.py
@@ -1,11 +1,12 @@
 import pytest
 import random
+from common.utils.file_utils import generate_fake_file
 from ..models import DocumentType, Document
-from .factories import DocumentTypeFactory, DocumentFactory
+from .factories import DocumentTypeFactory, DocumentFactory, FileDocumentFactory
 from ..enums import BaseDocumentTypesEnum
 from faker import Faker
 
-fake = Faker()
+FAKER = Faker()
 # factories
 
 
@@ -21,6 +22,19 @@ def factory_document_fixture(
     factory_doctor_fixture,
 ) -> Document:
     return DocumentFactory.create(
+        user=factory_user_with_role_patient_fixture,
+        document_type=factory_document_type_fixture,
+        creator=factory_doctor_fixture,
+    )
+
+
+@pytest.fixture
+def factory_file_document_fixture(
+    factory_user_with_role_patient_fixture,
+    factory_document_type_fixture,
+    factory_doctor_fixture,
+) -> FileDocumentFactory:
+    return FileDocumentFactory.create(
         user=factory_user_with_role_patient_fixture,
         document_type=factory_document_type_fixture,
         creator=factory_doctor_fixture,
@@ -44,8 +58,18 @@ def document_type_fixture() -> dict:
 @pytest.fixture
 def document_fixture(patient_fixture, document_type_fixture, doctor_fixture) -> dict:
     return {
-        "name": fake.pystr(),
-        "content": fake.text(max_nb_chars=10000),
+        "name": FAKER.pystr(),
+        "content": FAKER.text(max_nb_chars=10000),
+        "user": patient_fixture,
+        "document_type": document_type_fixture,
+        "creator": doctor_fixture,
+    }
+
+
+@pytest.fixture
+def file_document_fixture(patient_fixture, document_type_fixture, doctor_fixture):
+    return {
+        "document": generate_fake_file(),
         "user": patient_fixture,
         "document_type": document_type_fixture,
         "creator": doctor_fixture,
