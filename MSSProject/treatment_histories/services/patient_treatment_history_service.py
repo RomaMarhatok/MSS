612449f5f17cf4c71f6ec3c4643a46365a70cf6d
roma <roma.marhatok@yandex.by>
2023-04-28 15:40:33 +0300

    add comments

--- MSSProject/treatment_histories/services/patient_treatment_history_service.py
+++ MSSProject/treatment_histories/services/patient_treatment_history_service.py
@@ -6,8 +6,11 @@ class PatientTreatmentHistoryService(BaseTreatmentHistoryService):
     def get_user_treatments_histories_list(
         self, patient_slug: str, request
     ) -> JsonResponse:
+        # проверка существования пользователя
         if self.is_user_exist(patient_slug):
+            # запрос лечеьбных историй пользователя
             treatments_histories = self.list(patient_slug, request=request)
+        # формирование ответа
         return JsonResponse(
             data={
                 "treatment_histories": treatments_histories,
