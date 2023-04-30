525bcdf0368385eb4ae54a0836ecea3aac17f759
roma <roma.marhatok@yandex.by>
2023-04-30 15:14:56 +0300

    chnage styles for add documents button

--- mssfront/src/view/doctor/documents/DoctorDocumentListView.vue
+++ mssfront/src/view/doctor/documents/DoctorDocumentListView.vue
@@ -155,7 +155,7 @@ onMounted(() => {
                     <RadioButton v-model="DOCUMENT_ORDER_FILTER" name="date_order" value="date" />
                 </div>
                 <div class="p-2">
-                    <button @click="addDocumentRedicrect">Добавить документ</button>
+                    <button @click="addDocumentRedicrect" class="add-document-button">Добавить документ</button>
                 </div>
             </section>
         </aside>
@@ -261,5 +261,56 @@ onMounted(() => {
 .create-document__link:hover {
     transition: 0.2s;
     color: rgba(19, 48, 94, 1);
+}
+
+.add-document-button {
+    appearance: none;
+    background-color: #38cf63;
+    border: 1px solid rgba(27, 31, 35, .15);
+    border-radius: 6px;
+    box-shadow: rgba(27, 31, 35, .1) 0 1px 0;
+    box-sizing: border-box;
+    color: #fff;
+    cursor: pointer;
+    display: inline-block;
+    font-family: -apple-system, system-ui, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji";
+    font-size: 14px;
+    font-weight: 600;
+    line-height: 20px;
+    padding: 6px 16px;
+    position: relative;
+    text-align: center;
+    text-decoration: none;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+    vertical-align: middle;
+    white-space: nowrap;
+}
+
+.add-document-button:focus:not(:focus-visible):not(.focus-visible) {
+    box-shadow: none;
+    outline: none;
+}
+
+.add-document-button:hover {
+    background-color: #2c974b;
+}
+
+.add-document-button:focus {
+    box-shadow: rgba(46, 164, 79, .4) 0 0 0 3px;
+    outline: none;
+}
+
+.add-document-button:disabled {
+    background-color: #94d3a2;
+    border-color: rgba(27, 31, 35, .1);
+    color: rgba(255, 255, 255, .8);
+    cursor: default;
+}
+
+.add-document-button:active {
+    background-color: #298e46;
+    box-shadow: rgba(20, 70, 32, .2) 0 1px 0 inset;
 }
 </style>
